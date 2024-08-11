e1ccbab5e7dcc381446b188a0fa9b2cd8612141d
sairlseb <cyrilseberiaga@gmail.com>
2024-08-11 14:37:26 +0800

    feature/live-birth-crud
    - added live birth page
    - added a table for live birth

---
+++ frontend/src/components/tables/live-birth-table.vue
@@ -0,0 +1,21 @@
+<template>
+    <DataTable>
+        <template #header>
+            <div class="flex justify-end mb-5">
+                <InputText placeholder="Search" class="w-96" />
+            </div>
+        </template>
+        <Column v-for="header in headers" :key="header.field" :header="header.header" :field="header.field"></Column>
+    </DataTable>
+</template>
+
+<script setup>
+
+const headers = [
+    { field: 'filename', header: 'Filename'},
+    { field: 'type', header: 'Type'},
+    { field: 'date_requested', header: 'Date Requested'},
+    { field: 'status', header: 'Status'},
+    { field: 'actions', header: 'Actions'}
+]
+</script>
---
+++ frontend/src/views/pages/LiveBirth.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Card>
+            <template #content>
+                <div class="flex flex-col px-5 py-5">
+                    <div class="flex justify-between items-center">
+                        <h1 class="text-2xl font-semibold">Live Birth</h1>
+                        <Button type="button" label="New" icon="pi pi-plus"></Button>
+                    </div>
+                    <div class="mt-5">
+                        <LiveBirthTable />
+                    </div>
+                </div> 
+            </template>
+        </Card>
+    </div>
+</template>
+
+
+<script setup>
+import LiveBirthTable from '@/components/tables/live-birth-table.vue';
+</script>
